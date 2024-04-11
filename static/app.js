6a8fb72c214169ee376831ddd6457e6c314faeaa
Sena Lee <dltpsk03@gmail.com>
2024-04-11 14:28:45 +0900

    test11

--- static/app.js
+++ static/app.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         loadingDiv.classList.remove('hidden'); // 로딩 메시지 표시
 
-        const url = 'http://139.84.134.82:22/identify';
+        const url = 'http://139.84.134.82:8080/identify';
         const formData = new FormData();
         formData.append('image', file);
 
